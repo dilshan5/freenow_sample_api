ee6915f25b56c9f541f1dc24237723cfed5ce51d
Dilshan F <dilshan.fernando@sysco.com>
2020-03-16 13:39:03 +0530

    fix issue in testEmailVerificationInComments

--- src/test/java/com/freenow/sample/api/tests/e2e/E2EWorkFlowTest.java
+++ src/test/java/com/freenow/sample/api/tests/e2e/E2EWorkFlowTest.java
@@ -4,6 +4,7 @@ import com.freenow.sample.api.tests.functionalTest.CommentsDetailTest;
 import com.freenow.sample.api.tests.functionalTest.PostDetailTest;
 import com.freenow.sample.api.tests.functionalTest.UserDetailTest;
 import com.freenow.sample.api.util.TestBase;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -14,7 +15,7 @@ public class E2EWorkFlowTest extends TestBase {
 
     @Parameters({"userName"})
     @Test(description = "ID-005")
-    public static void testEmailVerificationInComments(String userName) {
+    public static void testEmailVerificationInComments(@Optional("Samantha") String userName) {
         //Get userID
         UserDetailTest.testUserSearchByValidName(userName);
         //Get all the Post IDs which has been posted by above user
